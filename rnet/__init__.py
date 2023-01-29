1f63e915c76cbfd634d2e34bc1d0fb9f1193a87f
Kota Sakazaki <kotasakazaki@gmail.com>
2023-01-29 12:02:42 +0900

    Rename test.osm to shinjuku.osm

--- rnet/__init__.py
+++ rnet/__init__.py
@@ -1,6 +1,6 @@
 import os
 from osgeo import ogr
-ogr.Open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources', 'test.osm'))
+ogr.Open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'resources', 'shinjuku.osm'))
 
 from rnet.coordinates import *
 from rnet.model import *
