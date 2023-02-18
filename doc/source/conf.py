86874016b160ab753cc51be8dceeb4223cfb61fd
Kota Sakazaki <kotasakazaki@gmail.com>
2023-02-18 16:28:30 +0900

    Bump version to '0.0.7'

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Kota Sakazaki'
 author = 'Kota Sakazaki'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.6'
+release = '0.0.7'
 
 
 # -- General configuration ---------------------------------------------------
